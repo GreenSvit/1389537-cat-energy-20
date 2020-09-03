08a93ec6dd7931e9ce5d9da465936649b776729c
Roman <outpacroman@gmail.com>
2020-09-04 00:33:41 +0300

    Animation for menu

--- source/js/script.js
+++ source/js/script.js
@@ -1,22 +1,25 @@
 var menuToggle = document.querySelector('.menu-toggle');
-var menu = document.querySelector('.main-nav');
+var menu = document.querySelector('.page-header__nav');
 var headerWrapper = document.querySelector('.page-header__wrapper');
 
-
-menu.classList.add('main-nav--closed');
+menu.classList.add('main-nav--closed', 'main-nav--closed-animation');
 menuToggle.classList.remove('menu-toggle--active');
 headerWrapper.classList.remove('page-header__wrapper--active-menu');
 
-menuToggle.addEventListener('click', function (evt) {
-    if (!menuToggle.classList.contains('menu-toggle--active')) {
-        menu.classList.remove('main-nav--closed');
-        menuToggle.classList.add('menu-toggle--active');
-        headerWrapper.classList.add('page-header__wrapper--active-menu');
-    }
-
-    else {
-        menu.classList.add('main-nav--closed');
-        menuToggle.classList.remove('menu-toggle--active');
-        headerWrapper.classList.remove('page-header__wrapper--active-menu');
-    }
-})
+menuToggle.addEventListener('click', function(evt) {
+  if (!menuToggle.classList.contains('menu-toggle--active')) {
+    menu.classList.remove('main-nav--closed');
+    setTimeout(() => {
+      menu.classList.remove('main-nav--closed-animation');
+    }, 1);
+    menuToggle.classList.add('menu-toggle--active');
+    headerWrapper.classList.add('page-header__wrapper--active-menu');
+  } else {
+    menu.classList.add('main-nav--closed-animation');
+    setTimeout(() => {
+      menu.classList.add('main-nav--closed');
+    }, 500);
+    menuToggle.classList.remove('menu-toggle--active');
+    headerWrapper.classList.remove('page-header__wrapper--active-menu');
+  }
+});
